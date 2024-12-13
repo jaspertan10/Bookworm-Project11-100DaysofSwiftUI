0abce622ae2fff596fc87976d99abb9391ef8c89
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-12 22:31:04 -0800

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jasper Tan on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
