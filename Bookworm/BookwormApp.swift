e610234ef23269e8ddbb0cbb2d42d460fee6f490
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-18 20:14:46 -0800

    Add Add book feature, Rating View, and display cooks on ContentView

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
-@main
-struct BookwormApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+    @main
+    struct BookwormApp: App {
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+            .modelContainer(for: Book.self)
         }
     }
-}
